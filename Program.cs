99c9d8d5016fec70444d20db2bd7687d16cd9a86
WRayz <ruizhouwu@gmail.com>
2020-04-19 01:21:25 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BluSenseConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
